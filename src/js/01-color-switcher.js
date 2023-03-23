b76ee9d1349af04088eb3d0e92ef5f4b256751d6
RuslanaSemenenko <113388430+RuslanaSemenenko@users.noreply.github.com>
2023-03-23 21:24:23 +0000

    js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,25 +1,23 @@
-const btnStart = document.querySelector('button[data-start]');
-const btnStop = document.querySelector('button[data-stop]');
 
-btnStop.disabled = true;
-let colorInterval = null;
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
 
-btnStart.addEventListener('click', () => {
-  btnStart.disabled = true;
-  btnStop.disabled = false;
+let intervalId;
 
-  colorInterval = setInterval(() => {
-    document.body.style.background = getRandomHexColor();
+startButton.addEventListener('click', () => {
+  startButton.disabled = true;
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
   }, 1000);
 });
 
-btnStop.addEventListener('click', () => {
-  clearInterval(colorInterval);
-
-  btnStart.disabled = false;
-  btnStop.disabled = true;
+stopButton.addEventListener('click', () => {
+  startButton.disabled = false;
+  clearInterval(intervalId);
 });
